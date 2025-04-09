dfd57ee0efd490cc9775731a1fb1811874440b87
prabath1998 <prabhathudayanga1998@gmail.com>
2025-04-09 22:01:58 +0530

    setup cronjobs

---
+++ src/lib/cron.js
@@ -0,0 +1,18 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Cron job executed successfully");
+      } else {
+        console.error(`Cron job failed with status code: ${res.statusCode}`);
+      }
+    })
+    .on("error", (err) => {
+      console.error(`Error executing cron job: ${err.message}`);
+    });
+});
+
+export default job;
